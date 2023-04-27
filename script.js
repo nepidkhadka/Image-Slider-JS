98b15fcb17e94eb4ff491230e32cb460de5e8b68
Dipen Khadka <dipenkhadka191@gmail.com>
2023-04-27 19:10:14 +0545

    Made Responsive For All Screen Size

--- script.js
+++ script.js
@@ -1,39 +1,33 @@
 const images = document.querySelectorAll(".image-slider img");
 
-
 const nextbtn = document.querySelector(".left");
 const prevbtn = document.querySelector(".right");
 
 let index = 0;
-let width = images[index].clientWidth;
+// let width = images[index].clientWidth;
 
-nextbtn.addEventListener('click', ()=>{
-    index++;
-    slideimg();
-    if(index === images.length -1){
-        nextbtn.classList.add("disabled");
-    }else{
-        prevbtn.classList.remove("disabled");
-    }
-   
-})
+nextbtn.addEventListener("click", () => {
+  index++;
+  slideimg();
+  if (index === images.length - 1) {
+    nextbtn.classList.add("disabled");
+  } else {
+    prevbtn.classList.remove("disabled");
+  }
+});
 
+prevbtn.addEventListener("click", () => {
+  index--;
+  slideimg();
+  if (index === 0) {
+    prevbtn.classList.add("disabled");
+  } else {
+    nextbtn.classList.remove("disabled");
+  }
+});
 
-prevbtn.addEventListener('click', ()=>{
-    index--;
-    slideimg();
-    if(index === 0){
-        prevbtn.classList.add("disabled");
-    }else{
-        nextbtn.classList.remove("disabled");
-    }
-    
-})
-
-const slideimg = () =>{
-    images.forEach(
-        (slide)=>{
-            slide.style.transform = `translate(${-index * 100}%)`;  
-        }
-    )
-}
+const slideimg = () => {
+  images.forEach((slide) => {
+    slide.style.transform = `translate(${-index * 100}%)`;
+  });
+};
